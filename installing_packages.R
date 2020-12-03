38d8490003c401d3570ab59e7e4645766dd2e94a
williamorim <thewilliam89@gmail.com>
2020-12-03 15:01:40 -0300

    fixing runner and github package

--- installing_packages.R
+++ installing_packages.R
@@ -1,8 +1,11 @@
-install.packages(c("dplyr", "lubridate", "tidyverse", "reshape2", "ggplot2", "shiny",
-  "shinyBS", "shinyWidgets", "shinythemes", "plotly", "shinycssloaders",
-  "sf", "rgdal", "leaflet", "leaflet.extras", "tigris", "spdplyr",
-  "ggdendro", "scales", "shinyalert", "shinybusy", "RcppRoll",
-  "zoo", "readxl", "Jmisc", "cowplot", "survival", "reticulate", "devtools"
-))
+install.packages(
+  c("dplyr", "lubridate", "tidyverse", "reshape2", "ggplot2", "shiny",
+    "shinyBS", "shinyWidgets", "shinythemes", "plotly", "shinycssloaders",
+    "sf", "rgdal", "leaflet", "leaflet.extras", "tigris", "spdplyr",
+    "ggdendro", "scales", "shinyalert", "shinybusy", "RcppRoll",
+    "zoo", "readxl", "Jmisc", "cowplot", "survival", "reticulate", "devtools"
+  ),
+  repos = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest"
+)
 
-devtools::install_github('flipTime')
+devtools::install_github('isplayr/flipTime')
