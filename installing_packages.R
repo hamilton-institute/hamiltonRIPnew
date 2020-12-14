ac0fe178a5c643bd624fd38d9f169d3fe9301c99
Athos Petri Damiani <athos.damiani@gmail.com>
2020-12-13 23:29:58 -0300

    Update installing_packages.R

--- installing_packages.R
+++ installing_packages.R
@@ -1,6 +1,5 @@
 install.packages(
   c("dplyr", "lubridate", "tidyverse", "reshape2", "ggplot2", "shiny",
-    "usethis",
     "shinyBS", "shinyWidgets", "shinythemes", "plotly", "shinycssloaders",
     "rgdal", "leaflet", "leaflet.extras", "tigris", "spdplyr",
     "ggdendro", "scales", "shinyalert", "shinybusy", "RcppRoll",
@@ -8,7 +7,5 @@ install.packages(
   ),
   repos = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest"
 )
-
-install.packages(c("sf", "rsconnect"))
 
 devtools::install_github('Displayr/flipTime')
